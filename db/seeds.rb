ff63ca9e01aff0da66b08599307a83e00cc81f62
Oimah <obiora@dealdey.com>
2016-01-06 12:43:31 +0000

    Creating product, making it prettier and validation

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+# . . .
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'http://i.telegraph.co.uk/multimedia/archive/03387/iPhone_7_Nickolay__3387906k.jpg',
+price: 49.95)
+
+Product.create!(title: 'Rails 4 way',
+description:
+%{<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>},
+image_url: 'http://i.telegraph.co.uk/multimedia/archive/03387/iPhone_7_Nickolay__3387906k.jpg',
+price: 100.95)
