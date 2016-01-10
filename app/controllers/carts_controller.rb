964cc5df640e35cd751aaa6e8910b2f03dffb3e6
Oimah <obiora@dealdey.com>
2016-01-10 18:34:55 +0100

    Sending mail commit

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -55,23 +55,23 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_cart
       @cart = Cart.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def cart_params
       params[:cart]
     end
+    
 
     private
     def invalid_cart
@@ -79,13 +79,5 @@ class CartsController < ApplicationController
       redirect_to store_url, notice: 'Invalid cart'
     end
 
-    def destroy
-      @cart.destroy if @cart.id == session[:cart_id]
-      session[:cart_id] = nil
-      respond_to do |format|
-        format.html { redirect_to store_url,
-        notice: 'Your cart is currently empty' }
-        format.json { head :no_content }
-      end
-    end
+   
 end
